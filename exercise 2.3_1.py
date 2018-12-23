1b2b126bc03a6250557ca9a2514b13cf3b1444e9
manojlambo <43609845+manojlambo@users.noreply.github.com>
2018-12-23 20:43:58 +0530

    Add files via upload

---
+++ exercise 2.3_1.py
@@ -0,0 +1,7 @@
+"""
+The volume of a sphere with radius r. What is the volume of a sphere with radius 5
+"""
+pi=3.14
+r=float(input("Enter the radius of sphere : "))
+volume=(4/3)*pi*r**3
+print("the volume of sphere is : ",volume)
